9e66198f36d634385d08907bd7fe5c96acf5c15e
Alejandro Bertinelli <alexberti02@gmail.com>
2018-12-31 15:07:11 +0100

    Actualizaciones de estilo
    Actualizaciones de estilo y funcionamiento de la página

--- pilarbellosillo/public/scripts.js
+++ pilarbellosillo/public/scripts.js
@@ -1,15 +1,32 @@
 window.onscroll = function() {funcProg()};
 
-var navbar;
-var sticky;
+var navbar, titulo;
+var stickyN, stickyT;
 document.addEventListener('DOMContentLoaded', function(){
 	navbar = document.getElementById("navbar");
-	sticky = navbar.offsetTop;
+	titulo = document.getElementById("titulo");
+	stickyN = navbar.offsetTop;
+	stickyT = titulo.offsetTop;
 })
 function funcProg() {
-	if (window.pageYOffset >= sticky) {
+	if (pageYOffset >= stickyN) {
 		navbar.classList.add("sticky")
-	  } else {
+	} else {
 		navbar.classList.remove("sticky");
-	  }
+	}
+	if (pageYOffset >= stickyT) {
+		titulo.style.position = "fixed"
+		titulo.style.top = 0
+	} else {
+		titulo.style.position = "relative"
+		titulo.style.top = "50%"
+	}
+	if(pageYOffset >= stickyT * 1.1 && pageYOffset <= stickyN){
+		var k = 1+(stickyT - pageYOffset-64)*2/stickyN
+		console.log(k)
+		titulo.style.opacity = k
+	}
+	console.log("Offset: " + pageYOffset)
+	console.log("stN: " +stickyN)
+	console.log("stT: " +stickyT)
 }
