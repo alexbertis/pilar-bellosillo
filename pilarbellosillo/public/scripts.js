c5397777bc0141d9c71b91fd45607ab3f0843bc6
alexbertis <alexberti02@gmail.com>
2019-07-18 22:24:37 +0200

    + Contenido

--- pilarbellosillo/public/scripts.js
+++ pilarbellosillo/public/scripts.js
@@ -2,6 +2,9 @@ window.onscroll = function() {funcProg()};
 
 var nav_top, nav_info, nav_2, nav_3;
 var nav_items, part_items;
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
 	nav_top = document.getElementById("n_top")
 	nav_info = document.getElementById("n_info")
@@ -10,28 +13,29 @@ document.addEventListener('DOMContentLoaded', function(){
 	nav_items = [nav_top, nav_info, nav_2, nav_3]
 	part_items = [document.getElementById("top"),document.getElementById("vida"),document.getElementById("vida2"),document.getElementById("vida3")]
 
+
+	var btn = document.getElementById("btInfoPlanesAC")
+	var sh = document.getElementById("sh")
+	var flechaaarriba = document.createElement("i")
+	flechaaarriba.className = "material-icons"
+	flechaaarriba.innerText = "expand_less"
+	var flechaaabajo = document.createElement("i")
+	flechaaabajo.className = "material-icons"
+	flechaaabajo.innerText = "expand_more"
+	btn.onclick = function() {
+		if(sh.style.maxHeight == "0px"){
+			sh.style.maxHeight = "800px";
+			btn.text = "Menos información"
+			btn.appendChild(flechaaarriba)
+		}else{
+			sh.style.maxHeight = "0px";
+			btn.text = "Ver los planes destacados"
+			btn.appendChild(flechaaabajo)
+		} 
+	}
+	
 })
 function funcProg() {
-	/*if (pageYOffset >= stickyN) {
-		navbar.classList.add("sticky")
-	} else {
-		navbar.classList.remove("sticky");
-	}
-	if (pageYOffset >= stickyT) {
-		titulo.style.position = "fixed"
-		titulo.style.top = 0
-	} else {
-		titulo.style.position = "relative"
-		titulo.style.top = "50%"
-	}
-	if(pageYOffset >= stickyT * 1.1 && pageYOffset <= stickyN){
-		var k = 1+(stickyT - pageYOffset-64)*2/stickyN
-		console.log(k)
-		titulo.style.opacity = k
-	}
-	console.log("Offset: " + pageYOffset)
-	console.log("stN: " +stickyN)
-	console.log("stT: " +stickyT)*/
 	part_items.forEach(element => {
 		if(pageYOffset >= element.offsetTop){
 			nav_items.forEach(nava => {
