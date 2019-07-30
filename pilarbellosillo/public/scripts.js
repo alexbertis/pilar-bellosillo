695357b7b1d910d59a5c7f9ec50f44805c18e850
alexbertis <alexberti02@gmail.com>
2019-07-30 20:24:24 +0200

    Cambios en testimonio

--- pilarbellosillo/public/scripts.js
+++ pilarbellosillo/public/scripts.js
@@ -1,41 +1,8 @@
 window.onscroll = function() {funcProg()};
 
-var nav_top, nav_info, nav_2, nav_3, nav_4;
 var nav_items, part_items;
 
 
-
-document.addEventListener('DOMContentLoaded', function(){
-	nav_top = document.getElementById("n_top")
-	nav_info = document.getElementById("n_info")
-	nav_2 = document.getElementById("n_umofc")
-	nav_3 = document.getElementById("n_define")
-	nav_4 = document.getElementById("n_cont")
-	nav_items = [nav_top, nav_info, nav_2, nav_3, nav_4]
-	part_items = [document.getElementById("top"),document.getElementById("vida"),document.getElementById("etapaInter"),document.getElementById("ladefine"),document.getElementById("contacto")]
-
-
-	var btn = document.getElementById("btInfoPlanesAC")
-	var sh = document.getElementById("sh")
-	var flechaaarriba = document.createElement("i")
-	flechaaarriba.className = "material-icons"
-	flechaaarriba.innerText = "expand_less"
-	var flechaaabajo = document.createElement("i")
-	flechaaabajo.className = "material-icons"
-	flechaaabajo.innerText = "expand_more"
-	btn.onclick = function() {
-		if(sh.style.maxHeight == "0px"){
-			sh.style.maxHeight = "800px";
-			btn.text = "Menos información"
-			btn.appendChild(flechaaarriba)
-		}else{
-			sh.style.maxHeight = "0px";
-			btn.text = "Ver los planes destacados"
-			btn.appendChild(flechaaabajo)
-		} 
-	}
-	
-})
 function funcProg() {
 	part_items.forEach(element => {
 		if(pageYOffset >= element.offsetTop){
